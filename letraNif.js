f3aa0e5011b2a597883c2fea75de626b889e3a16
Pedro Frias <plfriasf@gmail.com>
2020-11-14 01:41:19 -0800

    first commit

---
+++ letraNif.js
@@ -0,0 +1,37 @@
+// Guess the letter of the identity card
+
+// The letter depends on the result of dividing the 8 digits number by 23. Each possible remainder of this division corresponds with a letter. More info at https://www.letranif.com/formula-para-calcular-nif/
+
+const DNI = 99999999;
+
+const letra = [
+  "T",
+  "R",
+  "W",
+  "A",
+  "G",
+  "M",
+  "Y",
+  "F",
+  "P",
+  "D",
+  "X",
+  "B",
+  "N",
+  "J",
+  "Z",
+  "S",
+  "Q",
+  "V",
+  "H",
+  "L",
+  "C",
+  "K",
+  "E",
+];
+
+function dniLetter(dniNumber) {
+  return dniNumber + letra[dniNumber % 23];
+}
+
+dniLetter(DNI);
